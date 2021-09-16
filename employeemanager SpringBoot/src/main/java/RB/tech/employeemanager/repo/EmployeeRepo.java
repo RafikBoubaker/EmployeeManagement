c5abbedde6970d93b4fee4f1445f453961d3c749
RafikBoubaker <boubaker.rafik@gmail.com>
2021-09-16 12:19:04 +0100

    Backend V0.1

---
+++ employeemanager SpringBoot/src/main/java/RB/tech/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,14 @@
+package RB.tech.employeemanager.repo;
+
+import RB.tech.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface EmployeeRepo extends JpaRepository<Employee , Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
